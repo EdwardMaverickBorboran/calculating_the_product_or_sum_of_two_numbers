079c91a3752e7f1ebc350f54c35e180e312eeef4
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-21 21:16:17 +0800

    First problem encountered. No errors found yet, the results won't show up on the terminal.

--- multiply_or_add.py
+++ multiply_or_add.py
@@ -11,12 +11,21 @@
 number_1 = 20
 number_2 = 30
 
-#Expected output for Given 1
-
+# Expected output for Given 1
 # The result is 600
 
 # Given 2
 number_3 = 30
 number_4 = 40
 
+# Expected output for Given 1
+# The result is 70
+
 # psuedocode
+def multiply_or_add(number_1, number_2):
+    product_of_two = number_1 * number_2
+    if product_of_two <= 1000:
+        return print("The results is " + product_of_two)
+    else:
+        sum_of_two = number_3 + number_4
+        return print("The results is " + sum_of_two)
