4665d58d90ba9b8b22f7b824d20eda8602e6a826
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-21 20:57:11 +0800

    assigning the given values and there expected results

---
+++ multiply_or_add.py
@@ -0,0 +1,22 @@
+# Create a program that will --
+
+# Calculate the product if and only the product of two number is equal to or greater than 1000
+# Else, add the two number to get their sum
+
+# DEADLINE: JANUARY 26, 2024
+
+# Assigning the given numbers and their expected output
+
+# Given 1
+number_1 = 20
+number_2 = 30
+
+#Expected output for Given 1
+
+# The result is 600
+
+# Given 2
+number_3 = 30
+number_4 = 40
+
+# psuedocode
